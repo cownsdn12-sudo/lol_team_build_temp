3a9a1a8718f8b0f3047a05d7118dba1368c3622d
cownsdn12-sudo <cownsdn12@gmail.com>
2026-05-10 16:13:27 +0900

    Update app.py

--- app.py
+++ app.py
@@ -269,3 +269,7 @@ if run_button:
     c3.metric("2팀 총점", f"{pick['team2']['team_total']:.2f}")
 
     st.dataframe(make_team_table(pick["team1"], pick["team2"]), use_container_width=True)
+
+# 준우 포지션 강제 지정
+df.loc[df["이름"] == "준우", "주포지션"] = "원딜"
+df.loc[df["이름"] == "준우", "부포지션"] = [["정글"]]
